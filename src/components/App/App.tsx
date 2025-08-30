9e1fb460d2eaad5146b19bb6a22a7c35245e0598
Maryna Tkachenko <marynapanchenko19@gmail.com>
2025-08-30 13:59:03 +0300

    hw done

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,7 @@ import css from './App.module.css'
 import type { Votes, VoteType } from '../../types/votes';
 import VoteOptions from '../VoteOptions/VoteOptions';
 import VoteStats from '../VoteStats/VoteStats';
+import Notification from '../Notification/Notification';
 
 
 
@@ -26,13 +27,24 @@ function App() {
     setVotes({ good: 0, neutral: 0, bad: 0 })
   }
 
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0
+
+    
   return (
  <div className={css.app}>
   <CafeInfo />
-  <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
-  <VoteStats votes={votes} totalVotes={0} positiveRate={0} />
+  <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={totalVotes > 0} />
+  {totalVotes !== 0
+    ? <VoteStats votes={votes} totalVotes={totalVotes} positiveRate={positiveRate} />
+    : <Notification />
+  } 
  </div>
   )
 }
+
 
 export default App;
